cf9b260d3994b0b6c22f9a565639525228b3ef24
ndelop <ndelop@gmail.com>
2017-10-20 16:30:17 +0200

    I plot, you plot

--- plot_face.m
+++ plot_face.m
@@ -7,7 +7,7 @@ if nargin < 2
 end
 
 scatter(face(1:2:end), face(2:2:end),opt);
-text(face(1:2:end), face(2:2:end),num2str([1:size(face)/2].'));
+%text(face(1:2:end)+0.5, face(2:2:end),num2str([1:size(face)/2].'));
 axis ij
 end
 
